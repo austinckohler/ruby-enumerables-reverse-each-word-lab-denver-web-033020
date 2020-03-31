196f7811a5ed12716665e3637e4777c27ade053f
Austin Kohler <austinckohler@gmail.com>
2020-03-31 18:56:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,27 +1,58 @@
+require "pry"
 def reverse_each_word(string)
-
+# binding.pry 
   array = string.split(" ")
-
+# binding.pry 
 
   array.each do |word|
     word.reverse!
-
+binding.pry
   end 
 
   array.join(" ")
 
 end 
 
-def reverse_each_word(string)
+# def reverse_each_word(string)
 
-  array = string.split(" ")
+#   array = string.split(" ")
 
 
-  array.collect do |word|
-    word.reverse!
+#   array.collect do |word|
+#     word.reverse!
 
-  end 
+#   end 
 
-  array.join(" ")
+#   array.join(" ")
 
-end 
+# end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
